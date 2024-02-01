96cba26a31f550bc0534d43cd6a0416fd7c89d3b
Will Fairclough <wfairclough@gmail.com>
2024-02-01 09:14:02 -0500

    Update lazygit keybindings

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,9 +1,13 @@
 return {
-    {
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-    },
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+
+		config = function()
+			vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
+		end,
+	},
 }
