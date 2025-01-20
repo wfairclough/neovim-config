ac62a02f107417d26af3d173716433e7243b2e27
Will Fairclough <github@willdoes.dev>
2025-01-20 12:52:25 -0500

    Update rg flags for telescope live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,20 @@ return {
     },
     config = function()
       require("telescope").setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob",
+            "!.git/", -- Exclude .git/ if desired
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
@@ -21,12 +35,12 @@ return {
         pickers = {
           find_files = {
             hidden = true,
-            git_ignore_files = false,
+            git_ignore = false,
           },
           live_grep = {
             hidden = true,
           },
-        }
+        },
       })
 
       require("telescope").load_extension("fzf")
@@ -34,11 +48,12 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-      vim.keymap.set("n", "<leader>/", "<leader>fg", { desc="Live Grep", remap = true })
-      vim.keymap.set("n", "<leader><space>", "<C-p>", { desc="Find Files", remap = true })
+      vim.keymap.set("n", "<leader>/", "<leader>fg", { desc = "Live Grep", remap = true })
+      vim.keymap.set("n", "<leader><space>", "<C-p>", { desc = "Find Files", remap = true })
       vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+      vim.keymap.set("n", "<leader>f?", builtin.help_tags, { desc = "Help Tags" })
 
       -- Find configuration files
       vim.keymap.set("n", "<leader>fc", function()
@@ -50,7 +65,7 @@ return {
       vim.keymap.set("n", "<leader>fp", function()
         builtin.find_files({
           prompt_title = "Find Packages",
-          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
         })
       end, { desc = "Find Packages" })
 
@@ -61,7 +76,6 @@ return {
       vim.keymap.set("n", "<leader>sd", builtin.lsp_definitions, { desc = "Search Definitions" })
       vim.keymap.set("n", "<leader>st", builtin.lsp_type_definitions, { desc = "Search Type Definitions" })
       vim.keymap.set("n", "<leader>si", builtin.lsp_implementations, { desc = "Search Implementations" })
-
 
       require("telescope").load_extension("ui-select")
     end,
